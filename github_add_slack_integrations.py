46fe84c6438e4ef4a53222393defba2d99b5afff
William Yardley <wby@axs.com>
2015-10-09 12:02:40 -0700

    Initial import

---
+++ github_add_slack_integrations.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+"""This tool takes in a JSON list of integrations in a file named
+integrations.json, formatted like:
+
+       { "channel1": { "slackToken": "NNNN/XXXX/YYYYYYYYYYZ",
+                       "repos": ["repo1", "repo2"]
+                      },
+         "channel2": { "slackToken": "NNNN/ZZZZ/DDDDDDDDDDD",
+                       "repos": ["repo1", "repo2"]
+                      }
+       } 
+
+You should also populate 'OWNER_NAME' at the top, and configure the file
+'.github_token' in your home directory with a github API token.
+"""
+__author__ = "William Yardley"
+
+import json
+import requests
+import sys
+from os.path import expanduser
+
+API_URL='https://api.github.com'
+SLACK_BASE_URL = 'https://hooks.slack.com/services/'
+OWNER_NAME=''
+
+def create_webhook(github_token, repo, slack_webhook):
+
+    call = '%s/repos/%s/%s/hooks' % (API_URL, OWNER_NAME, repo)
+    payload = { "name": "web",
+                "config": {
+                   "url": slack_webhook,
+                   "content_type": "json"
+                 }
+               }
+    response = requests.post(call, json=payload,
+          headers={'Authorization': github_token})
+    if response.status_code == 201:
+        print >> sys.stderr, "Added integration for %s" % repo
+    else:
+        errors = list()
+        for error in response.json()['errors']:
+            errors.append(error['message'])
+        print >> sys.stderr, "Error (repo %s): %s: %s (code %s)" % (repo,
+            response.json()['message'], ','.join(errors),
+            response.status_code)
+
+def main():
+
+    home = expanduser('~')
+    cfg = open(home + '/.github_token', 'r')
+    github_token = 'token ' + cfg.readlines()[0].rstrip()
+
+    with open('integrations.json') as data_file:    
+        integrations = json.load(data_file)
+
+    for channel, values in integrations.iteritems():
+        slack_webhook = SLACK_BASE_URL + values['slackToken']
+        for repo in values['repos']:
+            create_webhook(github_token, repo, slack_webhook)
+
+if __name__ == "__main__":
+    main()
---
+++ github_find_stale_integrations.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+
+"""
+You should configure the file '.github_token' in your home directory
+with a github API token. Additionally, Make sure to update 'OWNER_NAME'
+near the top.
+
+"""
+
+__author__ = "William Yardley"
+
+import argparse
+import json
+import requests
+import os
+import sys
+from os.path import expanduser
+
+API_URL='https://api.github.com'
+OWNER_NAME=''
+github_token=str()
+
+def _arg_parser():
+
+    filename = os.path.basename(__file__)
+    if filename.endswith('.pyc'):
+        filename = filename[:-1]
+
+    parser = argparse.ArgumentParser(prog=filename,
+       description="""This tool will try to find Github hooks matching
+       certain patterns, and, optionally, delete them.""")
+    parser.add_argument('-p', '--pattern', required=True,
+       help='''Literal pattern (not regexp) to look for in the URL (for
+            hipchat, just use "hipchat" as the pattern)''')
+    parser.add_argument('-d', '--delete', action='store_true',
+       default=False, help='Delete hooks, rather than just identifying them.')
+
+    return parser
+
+def get_github_token():
+
+    home = expanduser('~')
+    cfg = open(home + '/.github_token', 'r')
+    github_token = 'token ' + cfg.readlines()[0].rstrip()
+
+    return github_token
+
+
+def get_repos():
+
+    repos = list()
+    call = '%s/orgs/%s/repos' % (API_URL, OWNER_NAME)
+
+    i = 1
+    while True:
+        params = {'page':i, 'per_page':100}
+        response = requests.get(call, params=params,
+              headers={'Authorization': github_token})
+        if response.status_code == 200:
+            if len(response.json()) > 0:
+                for item in response.json():
+                    repos.append(item['name'])
+            else:
+                break
+        else:
+            print >> sys.stderr, "Got error %s" % response.status_code
+        i += 1
+
+    return repos
+
+
+def get_hooks(repo):
+
+    datadict = dict()
+    call = '%s/repos/%s/%s/hooks' % (API_URL, OWNER_NAME, repo)
+
+    response = requests.get(call,
+          headers={'Authorization': github_token})
+    for item in response.json():
+        if item['name'] == 'web':
+            item_id = item['id']
+            datadict[item_id] = item['config']['url']
+        elif item['name'] == 'hipchat':
+            item_id = item['id']
+            datadict[item_id] = 'hipchat'
+
+    return datadict
+
+def delete_hook(repo, hook_id):
+
+    call = '%s/repos/%s/%s/hooks/%s' % (API_URL, OWNER_NAME, repo, hook_id)
+    response = requests.delete(call, headers={'Authorization': github_token})
+    if response.status_code == 204:
+        print >> sys.stderr, "Deleted hook %s on repo %s" % (hook_id, repo)
+    else:
+        print >> sys.stderr, ("Error %s for hook %s on repo %s" %
+           (response.status_code, hook_id, repo))
+
+def main():
+
+    args = _p.parse_args()
+    global github_token
+    github_token=get_github_token()
+
+    repos = get_repos()
+    for repo in repos:
+        # hack to skip 'site-*' repos
+        if repo.startswith('site-'):
+            continue
+        hooks = get_hooks(repo)
+        for hook_id, url in hooks.iteritems():
+            if args.pattern in url:
+                if args.delete:
+                    delete_hook(repo, hook_id)
+                else:
+                    print "%s, %s, %s" % (repo, hook_id, url)
+
+# Build pydoc from argparser help
+_p = _arg_parser()
+__doc__ += _p.format_help()
+
+if __name__ == "__main__":
+    main()
